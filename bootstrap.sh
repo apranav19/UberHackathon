308e858c2e1e009ec9a94a20640fa68558d15c3b
Kevin Aloysius <kevinaloys@rocketmail.com>
2016-02-20 22:12:05 -0800

    fake factroy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,13 @@
 sudo apt-get update
-cd /home/
+sudo cd /home/
 sudo wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.5.0-Linux-x86_64.sh
-bash /home/Anaconda3-2.5.0-Linux-x86_64.sh -b -p /home/anaconda
+/bin/bash Anaconda3-2.5.0-Linux-x86_64.sh -b -p /home/anaconda
 export PATH="/home/anaconda/bin:$PATH"
 conda create -n UberHackathon -y python==3.4.3 pip
 source activate UberHackathon
 pip install flask
 pip install requests
 pip install fake-factory
-sudo apt-get install nodejs
-sudo apt-get install npm
 # export PATH="$HOME/anaconda/bin:$PATH"
 # conda create -n UberHackathon -y python==3.4.3 pip
 # source activate UberHackathon
