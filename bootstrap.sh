239bad7ab48ec2b736f44061a4918aefe187046d
Kevin Aloysius <kevinaloys@rocketmail.com>
2016-02-21 00:47:08 -0800

    commit

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo cd /home/
+sudo wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.5.0-Linux-x86_64.sh
+/bin/bash Anaconda3-2.5.0-Linux-x86_64.sh -b -p /home/anaconda
+export PATH="/home/anaconda/bin:$PATH"
+conda create -n UberHackathon -y python==3.4.3 pip
+source activate UberHackathon
+pip install flask
+pip install requests
+pip install fake-factory
+# export PATH="$HOME/anaconda/bin:$PATH"
+# conda create -n UberHackathon -y python==3.4.3 pip
+# source activate UberHackathon
+# pip install flask
+# pip install requests
